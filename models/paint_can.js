3148fc67357563d5f4c83c86194dfe5669b58ed6
John <ashton.basile@gmail.com>
2023-08-08 15:50:15 +0100

    I added some requirements

---
+++ models/paint_can.js
@@ -0,0 +1,7 @@
+/*
+A paint can should:
+
+have a number of litres of paint
+be able to check if it is empty
+be able to empty itself of paint
+*/
---
+++ models/room.js
@@ -0,0 +1,7 @@
+/*
+A room should:
+
+have an area in square meters
+should start not painted
+should be able to be painted
+*/
